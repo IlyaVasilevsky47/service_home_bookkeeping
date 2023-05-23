acddb8f79ec413ed965299ccdf5082308afaf24a
Илья Василевский <vasilevskijila047@gmail.com>
2023-05-23 13:39:26 +0300

    Version 3 workflows

--- home_bookkeeping/home_bookkeeping/settings.py
+++ home_bookkeeping/home_bookkeeping/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = os.getenv(
     'SECRET_KEY', default='whab*7btiqhag57=mqia+n3!h&m!(cwl5e+8#0azas@iy5hb*$'
 )
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -118,7 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
